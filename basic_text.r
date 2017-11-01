de773e8bd6464fc80f8712958214746e4708b8d7
kolvmod <32670136+oltkkol@users.noreply.github.com>
2017-11-01 15:51:00 +0100

    Update basic_text.r
    Better TokenizeText ... added mask and toLower

--- basic_text.r
+++ basic_text.r
@@ -50,8 +50,21 @@ MergeTexts <- function(seznamTextu){
 ## TOKENIZACE, TYPY, ATD.	#######################################################################
 
 # basic plain text tokenizer by regular expression split mask
-TokenizeText <- function(text, regexPattern="\\W+"){
-	tokens = tolower( unlist(stri_split_regex(text, regexPattern)) )
+# Eg.:	TokenizeText("Hey, this is an example")  returns "hey", "this", "is", "an", "example"
+#		TokenizeText("Hey, this is an example", regexPattern="\\b\\w{3}\\b", regexIsMask=TRUE) returns "hey"
+TokenizeText <- function(text, regexPattern="\\W+", regexIsMask=FALSE, convertToLowerCase=TRUE){
+	if (regexIsMask){
+		tokens = str_extract_all(text, regexPattern)
+	}else{
+		tokens = stri_split_regex(text, regexPattern)
+	}
+
+	tokens = unlist(tokens)
+
+	if (convertToLowerCase){
+		tokens = tolower(tokens)
+	}
+
 	tokens = tokens[ tokens != "" ]
 	return(tokens)
 }
