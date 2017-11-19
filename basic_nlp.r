2f51eea57f2fba1e9e1c912bad57b8190ba0ea60
oltkkol <32670136+oltkkol@users.noreply.github.com>
2017-11-19 15:35:04 +0100

    Minor fixes

--- basic_nlp.r
+++ basic_nlp.r
@@ -1,4 +1,4 @@
-## DEPENDENCY:
-## basic_text.r
-## basic_ml.r
+## DEPDENCY #######################################################################################
+source("https://raw.githubusercontent.com/oltkkol/vmod/master/basic_text.r", encoding="UTF-8")
+source("https://raw.githubusercontent.com/oltkkol/vmod/master/basic_ml.r", encoding="UTF-8")
 
