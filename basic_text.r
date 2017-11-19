b5683a113a0787ba2ad4a8e648286c5fd745930b
oltkkol <32670136+oltkkol@users.noreply.github.com>
2017-11-19 15:17:40 +0100

    Dependency enhancements

--- basic_text.r
+++ basic_text.r
@@ -1,15 +1,13 @@
 ## DEPENDENCY #####################################################################################
 source("https://raw.githubusercontent.com/oltkkol/vmod/master/rlibrary_dependency.r")
 
-installBiostrings <- function(){
-	source("https://bioconductor.org/biocLite.R")
-	biocLite("Biostrings")
-}
-
 rlibrary("stringi")
 rlibrary("proxy")
 rlibrary("stringdist")
-rlibrary("Biostrings", installBiostrings)
+rlibrary("Biostrings", function(){
+	source("https://bioconductor.org/biocLite.R")
+	biocLite("Biostrings")
+})
 
 Sys.setlocale(category="LC_ALL", locale = "English_United States.1252")
 Sys.getlocale(category="LC_ALL")
