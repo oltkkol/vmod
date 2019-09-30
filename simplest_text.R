3556b0ccb0e4f800e6a289c8fca6133d39973f87
kolvmod <32670136+oltkkol@users.noreply.github.com>
2019-09-30 12:36:22 +0200

    Update simplest_text.R

--- simplest_text.R
+++ simplest_text.R
@@ -27,3 +27,16 @@ TokenizeText <- function(text, regexPattern="\\W+", regexIsMask=FALSE, convertTo
 	tokens = tokens[ tokens != "" ]
 	return(tokens)
 }
+
+# Makes n-grams from tokens.
+# If Glue is a string, vector of n-gram strings are returned, otherwise a matrix
+# Eg.: MakeNGrams( c("a", "b", "c", "d", "e", "f", "g"), n=3, glue=" ")
+MakeNGrams <- function(tokens, n=2, glue="->"){
+	m <- t( sapply( 1:(length(tokens)-n+1), function(i) tokens[i:(i+n-1)] ) )
+
+	if (is.character(glue)){
+		m <- apply(m, 1, function(r) paste(r, collapse=glue))
+	}
+
+	return( m )
+}
